80edb8e2ae8fa8f4b6d7dabc5313f4d4326784f8
Evan Prenger <47509998+eprenger@users.noreply.github.com>
2021-10-07 21:41:49 -0400

    added slider coloration

--- app.js
+++ app.js
@@ -28,6 +28,14 @@ function randomColors() {
         hexText.innerText = randomColor;
         //check for contrast
         checkTextContrast(randomColor, hexText);
+        //initialize colorize sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll('.sliders input');
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color,hue,brightness,saturation);
     });
 }
 
@@ -38,6 +46,21 @@ function checkTextContrast(color,text){
     }else{
         text.style.color = "white";
     }
+}
+
+function colorizeSliders(color,hue,brightness,saturation) {
+    //scale saturation
+    const noSat = color.set('hsl.s',0);
+    const fullSat = color.set('hsl.s', 1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+    //scale brightness
+    const midBright = color.set('hsl.l',0.5);
+    const scaleBright = chroma.scale(['black', midBright, 'white']);
+
+    //update input colors
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)},${scaleBright(0.5)} ,${scaleBright(1)})`;
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75),rgb(75,204,75), rgb(75,204,204), rgb(75,57,204), rgb(204,75,204), rgb(204,75,75))`
 }
 
 randomColors();
