1923c9cf5f20767e981a6003e238a99c5f377f0b
Evan Prenger <47509998+eprenger@users.noreply.github.com>
2021-10-07 20:20:17 -0400

    added cdn and basic js functionality

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//colors app
+//global selections and variables
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+let initialColors;
+
+
+//functions
+
+
+//color generator
+function generateHex(){
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
+let randomHex = generateHex();
+console.log(randomHex);
+
+//random colors
+
+function randomColors() {
+    colorDivs.forEach((div,index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+
+
+        //add color to background
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+    });
+}
+
+randomColors();
